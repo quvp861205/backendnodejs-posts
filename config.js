a00169f8061a86351d2014fe069648bbe93b507b
unknown <quvp861205@gmail.com>
2021-01-17 15:55:31 -0700

    Configuracion con vercel para publicar este proyecto en internet

--- config.js
+++ config.js
@@ -6,17 +6,17 @@ module.exports = {
         secret: process.env.JWT_SECRET || "notasecret"
     },
     mysql: {
-        host: process.env.MYSQL_HOST || '127.0.0.1',
-        user: process.env.MYSQL_USER || 'root',
-        password: process.env.MYSQL_PASS || '',
-        database: process.env.MYSQL_DB || 'redsocial',
+        host: process.env.MYSQL_HOST || 'remotemysql.com',
+        user: process.env.MYSQL_USER || 'HUYxV1ZkCw',
+        password: process.env.MYSQL_PASS || 'IkrDSlMMZa',
+        database: process.env.MYSQL_DB || 'HUYxV1ZkCw',
 
     },
     mysql_service: {
         port: process.env.MYSQL_SRV_PORT || 3001,
         host: process.env.MYSQL_SRV_HOST || 'localhost',
     },
-    remoteDB: true,
+    remoteDB: process.env.REMOTE_DB || false,
     post: {
         port: process.env.POST_PORT || 3002,
     }
